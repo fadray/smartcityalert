793ca09ddf6291e23ff74080c0655f4517834278
Olubukola Fadaiya - Necasoft <olubukolafadaiya@gmail.com>
2026-06-19 14:30:10 +0100

    test page.tsx

--- admin-dashboard/app/page.tsx
+++ admin-dashboard/app/page.tsx
@@ -1,12 +1,7 @@
-'use client';
-
-import AuthWrapper from './providers/AuthWrapper';
-import HomePageContent from './components/HomePageContent';
-
 export default function Home() {
   return (
-    <AuthWrapper>
-      <HomePageContent />
-    </AuthWrapper>
+    <div>
+      <h1>SmartCity Alert Admin Test</h1>
+    </div>
   );
 }
